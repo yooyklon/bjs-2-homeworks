2021ac8459b782033eb2a13ed2310c65cd470967
Dmitry Sheshukov <yooyklon@gmail.com>
2021-11-15 20:45:39 +0700

    added function solveEquation and calculateTotalMortgage

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,53 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let x1;
+  let x2;
+  let arr = [];
+  let d = b ** 2 - 4 * a * c;
+  if (a == 0) {
+    console.log('Ошибка, a = 0 недопустимое значение!');
+  }
+  if (d > 0) {
+    x1 = (-b + Math.sqrt(d)) / (2 * a);
+    x2 = (-b - Math.sqrt(d)) / (2 * a);
+    arr.push(x1, x2);
+    return arr;
+  } else if (d == 0) {
+    x1 = -b / (2 * a);
+    arr.push(x1);
+    return arr;
+  } else if (d < 0) {
+    return arr;
+  }
+  return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
   let totalAmount;
+  let amountBody;
+  let monthlyPay;
+  let totalDate;
+  let current;
+  let months;
+  percent = Number(percent);
+  contribution = Number(contribution);
+  amount = Number(amount);
 
-  // код для задачи №2 писать здесь
+  if (percent > 60 || percent < 1) {
+    return 'Пожалуйста введите процентную ставку от 1 до 60';
+  } else if (contribution < 0 || contribution > amount) {
+    return 'Пожалуйчта введите корректный первоначальный взнос!';
+  } else if (amount <= 0) {
+    return 'Пожалуйчта введите корректную сумму кредита!';
+  }
 
-  return totalAmount;
+  amountBody = amount - contribution;
+  totalDate = new Date(date);
+  current = new Date();
+  months = (totalDate.getFullYear() - current.getFullYear())*12 + (totalDate.getMonth() - current.getMonth());
+  monthlyPay = amountBody * (percent/(100 * 12) + (percent/(100 * 12) / (((1 + percent/(100 * 12)) ** months) - 1)));
+  totalAmount = (monthlyPay * months);
+
+  return Number(totalAmount.toFixed(2));
 }
