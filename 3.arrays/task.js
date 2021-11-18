a2371acb259a0c15d9e62099cff912dfea553b74
Dmitry Sheshukov <yooyklon@gmail.com>
2021-11-18 18:52:22 +0700

    To the file 3.arrays/task.js added functio compareArrays and advancedFilter

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,20 @@
 function compareArrays(arr1, arr2) {
   let result;
+  result = arr1.every((item, inx, arr) => item === arr2[inx] && arr.length === arr2.length);
+ 
 
-  // Ваш код
-
-  return result; // boolean
+  return result;
 }
+
+let arr1 = [1, 2, 3];
+let arr2 = [1, 2, 3, 4];
+let x = compareArrays(arr1, arr2);
+console.log(x);
 
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr.filter(item => item > 0).filter(item => item % 3 == 0).map(item => item * 10)
 
-  return resultArr; // array
+  return resultArr; 
 }
