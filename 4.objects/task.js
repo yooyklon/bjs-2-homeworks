41833b92ec196e154a8821da51cace2d25639e85
Dmitry Sheshukov <yooyklon@gmail.com>
2021-12-05 22:02:19 +0700

    added function Student and prototype methods setSubject, addMark, addMarks, getAverage, exclude

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,46 @@
+'use strict';
+
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+Student.prototype.setSubject = function(subjectName) {
+  this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+    if ('marks' in this) {
+      this.marks.push(mark);
+    } else {
+      this.marks = [];
+      this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function(...marks) {
+  if ('marks' in this) {
+    for(let i = 0; i < marks.length; i++) {
+      this.marks.push(marks[i]);
+    }
+  } else {
+    this.marks = [];
+    for(let i = 0; i < marks.length; i++) {
+      this.marks.push(marks[i]);
+    }
+  }
+}
+
+Student.prototype.getAverage = function() {
+  let result = this.marks.reduce(function(sum, element) {
+    return sum + element;
+  }, 0);
+  return result / this.marks.length;
+}
+
+Student.prototype.exclude = function(reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+}
