7e78c7d620edae0806cdbb568bd818afa5f6cd37
Dmitry Sheshukov <yooyklon@gmail.com>
2021-12-25 01:40:09 +0700

    fixed methods addClock, removeClock, getCurentFormattedTime, start, stop and added method clearAlarms

--- 7.async/task.js
+++ 7.async/task.js
@@ -1,66 +1,81 @@
 class AlarmClock {
  constructor() {
   this.alarmCollection = [];
-  this.timerId;
+  this.timerId = null;
  }
 
  addClock(time, callback, id) {
 
-    if (!id) {
+  if (!id) {
       throw new Error('error text');
-    }
-  for (let i = 0; i < this.alarmCollection.length; i++) {
-    if (this.alarmCollection[i].id == id) {
-      console.error('Ошибка! звонок с таким id уже существует');
-      return;
-    }
   }
-  this.alarmCollection.push({id: id, time: time, callback: callback});
+  if (this.alarmCollection.find((item) => item.id == id)) {
+    console.error('Ошибка! звонок с таким id уже существует');
+    return;
+  }
+  this.alarmCollection.push({id, time, callback});
  }
 
  removeClock(id) {
-   for(let i = 0; i < this.alarmCollection.length; i++) {
-     if (this.alarmCollection[i].id === id) {
-       this.alarmCollection.splice(i, 1);
-       return true;
-     }
+   if (this.alarmCollection.find((item) => item.id == id)) {
+     this.alarmCollection.splice(this.alarmCollection.findIndex(item => item.id == id), 1);
+     return true;
+   } else {
      return false;
    }
  }
 
  getCurrentFormattedTime() {
    let date = new Date();
-   let result = date.getHours() + ':' + date.getMinutes();
+   let result = (date.getHours() < 10 ? '0' + date.getHours() : date.getHours()) + ':' + (date.getMinutes() < 10 ? '0' + date.getHours() : date.getMinutes());
    return result;
  }
 
  start() {
   if (!this.timerId) {
-    for(let i = 0; i < this.alarmCollection.length; i++) {
       this.timerId = setInterval(() => {
-        checkClock(this.alarmCollection[i]);
+        for(let i = 0; i < this.alarmCollection.length; i++) {
+          this.checkClock(this.alarmCollection[i]);
+        }
     }, 1000);
-    }
-  } else {
-    clearInterval(this.timerId);
   }
  }
 
  stop() {
   if (this.timerId) {
     clearInterval(this.timerId);
+    this.timerId = null;
   }
  }
 
  printAlarms() {
    this.alarmCollection.forEach(item => console.log(`${item.id} ${item.time}`));
  }
-}
+ clearAlarms() {
+   this.stop();
+   this.alarmCollection.length = 0;
+ }
 
-function checkClock(obj) {
-  let date = new Date();
-  let result = date.getHours() + ':' + date.getMinutes();
-  if (result == obj.time) {
+ checkClock(obj) {
+  if (this.getCurrentFormattedTime() == obj.time) {
     obj.callback();
    }
+ }
 }
+
+
+
+function testCase() {
+  let alarm = new AlarmClock();
+  alarm.addClock('01:19', () => console.log('Пора вставать!'), 1);
+  alarm.addClock('01:20', () => console.log('Пора вставать!'), 2);
+  alarm.removeClock(2);
+  alarm.addClock('01:21', () => console.log('Пора вставать!'), 3);
+  alarm.clearAlarms();
+  alarm.printAlarms();
+  alarm.addClock('01:21', () => console.log('Вставай а то проспишь!'), 1);
+  alarm.printAlarms();
+  alarm.start();
+}
+
+testCase();
