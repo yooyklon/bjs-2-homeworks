90faf4aeb2ccdab5142434478603c37108820be5
Dmitry Sheshukov <yooyklon@gmail.com>
2021-12-28 14:46:58 +0700

    added functions cachingDecoratorNew, debounceDecoratorNew and debounceDecorator2

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -1,12 +1,70 @@
+let add = (a, b, c) => console.log(a + b + c);
+
 function cachingDecoratorNew(func) {
-  // Ваш код
+  let cache = {};
+
+  function wrapper(...args) {
+    let hashe = args.join(',');
+
+    if (cache[hashe]) {
+      console.log('Из кэша: ' + cache[hashe]);
+      return 'Из кэша: ' + cache[hashe];
+    } else {
+      let result = func(...args);
+      cache[hashe] = result;
+      console.log('Вычисляем: ' + result);
+      wrapper.history.push(args);
+      if (wrapper.history.length == 6) {
+        let key = wrapper.history[0].join(',');
+        delete cache[key];
+        wrapper.history.splice(0, 1);
+      }
+      return 'Вычисляем: ' + result;
+    }
+  }
+  wrapper.history = [];
+  return wrapper;
 }
 
 
-function debounceDecoratorNew(func) {
-  // Ваш код
+function debounceDecoratorNew(func, ms) {
+  let timer;
+
+ function wrapper(...args) {
+
+  if (timer) {
+   clearTimeout(timer);
+   timer = setTimeout(() => {
+    func.call(this, ...args);
+   }, ms);
+  } else {
+   func(...args);
+   timer = setTimeout(() => {
+    func.call(this, ...args);
+   }, ms);
+  }
+ }
+ return wrapper;
 }
 
 function debounceDecorator2(func) {
-  // Ваш код
+  let timer;
+
+  function wrapper(...args) {
+
+   wrapper.count = wrapper.count + 1;
+   if (timer) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+     func.call(this, ...args);
+    }, ms);
+   } else {
+    func(...args);
+    timer = setTimeout(() => {
+     func.call(this, ...args);
+    }, ms);
+   }
+  }
+  wrapper.count = 0;
+  return wrapper;
 }
